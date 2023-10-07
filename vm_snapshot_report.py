0562ebff8929cba70f92c4832f5d235bda022aba
Marcos Fermin <marcosdavid1794@gmail.com>
2023-10-07 12:25:14 -0400

    Update vm_snapshot_report.py
    Made the table more responsive and with a red tone.

--- vm_snapshot_report.py
+++ vm_snapshot_report.py
@@ -5,7 +5,7 @@ Author: Marcos Fermin
 
 This script connects to vCenter, checks all VMs, and sends a report of
 all the remaining snapshots for each VM. It also sends an email if there
-are no snapshots found and highlights the snapshots that are older than 24 hours.
+are any snapshots older than 24 hours.
 """
 
 import ssl
@@ -26,10 +26,10 @@ if not vcenter_host or not vcenter_user or not vcenter_password:
     raise ValueError("VCENTER_HOST, VCENTER_USER, and VCENTER_PASSWORD environment variables must be set!")
 
 # Email settings
-smtp_server = 'your_smtp_server'
+smtp_server = 'mail-relay.cuny.edu'
 smtp_port = 25
-sender_email = 'your_sender_email'
-receiver_email = 'your_receiver_email'
+sender_email = 'sphtech@sph.cuny.edu'
+receiver_email = 'marcos.fermin@sph.cuny.edu'
 
 # Establish a connection to vCenter
 context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
@@ -85,15 +85,15 @@ if snapshot_data:
                 position: absolute;
                 top: -9999px;
                 left: -9999px;
-            }
-            tr { margin: 0 0 1rem 0; }
-            td {
+               }
+               tr { margin: 0 0 1rem 0; }
+               td {
                 border: none;
                 border-bottom: 1px solid #7f8c8d;
                 position: relative;
                 padding-left: 50%;
-            }
-            td:before {
+               }
+               td:before {
                 position: absolute;
                 top: 0;
                 left: 6px;
@@ -103,63 +103,184 @@ if snapshot_data:
                 content: attr(data-column);
                 color: #34495e;
                 font-weight: bold;
-            }
-        }
-    </style>
-    <div style="font-family: Arial, sans-serif; max-width: 800px; margin: 0 auto;">
-        <table style="border-collapse: collapse; width: 100%;">
-            <thead>
-                <tr>
-                    <th data-column="VM Name">VM Name</th>
-                    <th data-column="Snapshot Name">Snapshot Name</th>
-                    <th data-column="Description">Description</th>
-                    <th data-column="Created On">Created On</th>
-                    <th data-column="Age">Age</th>
-                </tr>
-            </thead>
-            <tbody>
-    """
+               }
+              }
+              table {
+               border-collapse: collapse;
+               width: 100%;
+               margin-bottom: 1rem;
+               background-color: transparent;
+               color: #212529;
+              }
+              table th,
+              table td {
+               padding: 0.75rem;
+               vertical-align: top;
+               border-top: 1px solid #dee2e6;
+              }
+              table thead th {
+               vertical-align: bottom;
+               border-bottom: 2px solid #dee2e6;
+              }
+              table tbody + tbody {
+               border-top: 2px solid #dee2e6;
+              }
+              table .table {
+               background-color: #fff;
+              }
+              table-sm th,
+              table-sm td {
+               padding: 0.3rem;
+              }
+              table-bordered {
+               border: 1px solid #dee2e6;
+              }
+              table-bordered th,
+              table-bordered td {
+               border: 1px solid #dee2e6;
+              }
+              table-bordered thead th,
+              table-bordered thead td {
+               border-bottom-width: 2px;
+              }
+              table-borderless th,
+              table-borderless td,
+              table-borderless thead th,
+              table-borderless tbody + tbody {
+               border: 0;
+              }
+              table-striped tbody tr:nth-of-type(odd) {
+               background-color: rgba(0, 0, 0, 0.05);
+              }
+              table-hover tbody tr:hover {
+               background-color: rgba(0, 0, 0, 0.075);
+              }
+              @media (max-width: 575.98px) {
+               table-responsive-sm {
+                display: block;
+                width: 100%;
+                overflow-x: auto;
+                -webkit-overflow-scrolling: touch;
+               }
+               table-responsive-sm > .table-bordered {
+                border: 0;
+               }
+              }
+              @media (max-width: 767.98px) {
+               table-responsive-md {
+                display: block;
+                width: 100%;
+                overflow-x: auto;
+                -webkit-overflow-scrolling: touch;
+               }
+               table-responsive-md > .table-bordered {
+                border: 0;
+               }
+              }
+              @media (max-width: 991.98px) {
+               table-responsive-lg {
+                display: block;
+                width: 100%;
+                overflow-x: auto;
+                -webkit-overflow-scrolling: touch;
+               }
+               table-responsive-lg > .table-bordered {
+                border: 0;
+               }
+              }
+              @media (max-width: 1199.98px) {
+               table-responsive-xl {
+                display: block;
+                width: 100%;
+                overflow-x: auto;
+                -webkit-overflow-scrolling: touch;
+               }
+               table-responsive-xl > .table-bordered {
+                border: 0;
+               }
+              }
+              /* New styles */
+              table {
+               background-color: #f8d7da;
+               color: #721c24;
+               font-weight: bold;
+              }
+              table th,
+              table td {
+               border: 1px solid #f5c6cb;
+               padding: 0.75rem;
+               vertical-align: top;
+              }
+              table thead th {
+               border-bottom: 2px solid #f5c6cb;
+              }
+              table tbody + tbody {
+               border-top: 2px solid #f5c6cb;
+              }
+              table-striped tbody tr:nth-of-type(odd) {
+               background-color: #fff3f4;
+              }
+              table-hover tbody tr:hover {
+               background-color: #f1b0b7;
+              }
+             </style>
+             <div style="font-family: Arial, sans-serif; max-width: 800px; margin: 0 auto;">
+              <div class="table-responsive">
+               <table class="table table-bordered table-striped table-hover">
+                <thead>
+                 <tr>
+                  <th>VM Name</th>
+                  <th>Snapshot Name</th>
+                  <th>Description</th>
+                  <th>Created On</th>
+                  <th>Age</th>
+                 </tr>
+                </thead>
+                <tbody>
+             """
     for snap in snapshot_data:
-        snapshot_report += f"""
-            <tr>
-                <td data-column="VM Name">{snap['vm_name']}</td>
-                <td data-column="Snapshot Name">{snap['name']}</td>
-                <td data-column="Description">{snap['description']}</td>
-                <td data-column="Created On">{snap['created_on']}</td>
-                <td data-column="Age">{snap['age']}</td>
-            </tr>
-        """
-    snapshot_report += "</tbody></table></div>"
+              snapshot_report += f"""
+               <tr>
+                <td>{snap['vm_name']}</td>
+                <td>{snap['name']}</td>
+                <td>{snap['description']}</td>
+                <td>{snap['created_on']}</td>
+                <td>{snap['age']}</td>
+               </tr>
+              """
+    snapshot_report += "</tbody></table></div></div>"
 else:
-    snapshot_report = '''
-    <style>
-        @media only screen and (max-width: 600px) {
-            div {
+             snapshot_report = '''
+             <style>
+              @media only screen and (max-width: 600px) {
+               div {
                 padding: 10px;
-            }
-            h1, p {
+               }
+               h1, p {
                 font-size: 18px;
-            }
-        }
-    </style>
-    <div style="border: 2px solid #e74c3c; padding: 20px; font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; text-align: center;">
-        <h1 style="color: #e74c3c;">No Snapshots Found!</h1>
-        <p style="font-size: 18px; color: #7f8c8d;">We couldn't find any snapshots for the VMs in the vCenter. It's always a good idea to regularly review and clean up unnecessary snapshots to save storage space and maintain VM performance.</p>
-    </div>
-    '''
+               }
+              }
+             </style>
+             <div style="border: 2px solid #e74c3c; padding: 20px; font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; text-align: center;">
+              <h1 style="color: #e74c3c;">No Snapshots Found!</h1>
+              <p style="font-size: 18px; color: #7f8c8d;">We couldn't find any snapshots for the VMs in the vCenter. It's always a good idea to regularly review and clean up unnecessary snapshots to save storage space and maintain VM performance.</p>
+             </div>
+             '''
 
-# Clean up and disconnect from vCenter
+            # Clean up and disconnect from vCenter
 container_view.Destroy()
 Disconnect(si)
 
-# Construct and send an email report
+            # Construct and send an email report
 subject = 'VM Snapshot Report'
 msg = MIMEText(snapshot_report, 'html')  
 msg['Subject'] = subject
 msg['From'] = sender_email
 msg['To'] = receiver_email
 
+            # Send the email
 with smtplib.SMTP(smtp_server, smtp_port) as server:
-    server.send_message(msg)
+             server.send_message(msg)
+             print('Email has been sent!')
 
-print('Email has been sent!')
+
